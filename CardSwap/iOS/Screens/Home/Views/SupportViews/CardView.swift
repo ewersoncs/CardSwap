0d80a49c6c31f4786b6e2f0be62819da1dd35196
Ewerson Castelo Silva <ewerson.castelo@gmail.com>
2025-06-17 22:20:00 -0400

    chore: create cardView

---
+++ CardSwap/iOS/Screens/Home/Views/SupportViews/CardView.swift
@@ -0,0 +1,19 @@
+//
+//  CardView.swift
+//  CardSwap
+//
+//  Created by Ewerson Castelo on 17/06/25.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
+c
